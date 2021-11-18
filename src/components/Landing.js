91783e70694255c303a16aac60ca863eef2ea7d1
allexib <ooomegasib@gmail.com>
2021-11-18 21:16:57 +0400

    4 restaraunts add

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import restaurants from '../sample-restaurants';
 
 class Landing extends React.Component {
     render() {
+
+
         return (
-            <div>
-                <div className='restaurant_select'>
-                    <div className='restaurant_select_top'>
-                        <div className='restaurant_select_top-header'>Выбери ресторан</div>
-                        <div className='arrow_picker'>
-                            <div className='arrow_picker-up'></div>
-                            <div className='arrow_picker-down'></div>
-                        </div>
+            <div className='restaurant_select'>
+                <div className='restaurant_select_top'>
+                    <div className='restaurant_select_top-header'>Выбери ресторан</div>
+                    <div className='arrow_picker'>
+                        <div className='arrow_picker-up'></div>
+                        <div className='arrow_picker-down'></div>
                     </div>
-                    <div className='restaurant_select_bottom'>
-                        <ul>
-                            <li>rest 1</li>
-                            <li>rest 2</li>
-                            <li>rest 3</li>
-                        </ul>
-                    </div>
+                </div>
+                <div className='restaurant_select_bottom'>
+                    <ul>
+                        {restaurants.map(restaurant => {
+                            return <li key={restaurant.id}>{restaurant.title}</li> ;
+                        })}
+                    </ul>
                 </div>
                 <button>Перейти в ресторан</button>
             </div>
