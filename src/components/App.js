985fc2deb18a4f22ce0812d0d7de7eb3db9fd731
allexib <ooomegasib@gmail.com>
2021-11-20 19:18:07 +0400

    14 loading all burgers in to State

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import MenuAdmin from './MenuAdmin';
+import sampleBurgers from '../sample-burgers';
 
-class App  extends React.Component{
+class App extends React.Component {
     state = {
         burgers: {},
         order: {}
@@ -11,22 +12,29 @@ class App  extends React.Component{
 
     addBurger = burger => {
         // 1. Делаем копию объекта state
-        const burgers = { ...this.state.burgers };
+        const burgers = {...this.state.burgers};
         // 2. Добавить новый бургер в переменную burgers
         burgers[`burger${Date.now()}`] = burger;
         // 3. Записать наш новый объект burgers в state
-        this.setState({ burgers });
+        this.setState({burgers});
+    };
+
+    loadSampleBurgers = () => {
+        this.setState({burgers: sampleBurgers});
     };
 
     render() {
-        return(
+        return (
             <div className='burger-paradise'>
                 <div className='menu'>
                     <Header title='Very Hot Burger' amount={10} hot={true}/>
 
                 </div>
                 <Order/>
-                <MenuAdmin addBurger={this.addBurger}/>
+                <MenuAdmin
+                    addBurger={this.addBurger}
+                    loadSampleBurgers={this.loadSampleBurgers}
+                />
             </div>
         );
     }
